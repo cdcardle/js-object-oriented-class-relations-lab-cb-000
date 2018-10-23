7c1154e9ed27230e7a3b0dc786edcd0807a602c1
Chris Cardle <cdcardle@gmail.com>
2018-10-23 16:39:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,4 +42,12 @@ class Trip {
 
     store.trips.push(this);
   }
+
+  driver() {
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId;
+      }.bind(this)
+    )
+  }
 }
