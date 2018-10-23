ad5d34a9b7d8cf4f3ae2b41d04a99ce830342a6d
Chris Cardle <cdcardle@gmail.com>
2018-10-23 16:22:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ let tripId = 0;
 
 class Trips {
   constructor(driver, passenger) {
-    this.driverId = driver.id;
-    this.passengerId = passenger.id;
+    this.driver = driver;
+    this.passenger = passenger;
   }
 }
